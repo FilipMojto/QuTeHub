bd95b4115272a6f9f16048230afade09cd21b59f
Filip Mojto <fmojto@gmail.com>
2024-07-18 20:55:07 +0200

    removing v2

--- database/DDL_query.sql
+++
@@ -1,9 +0,0 @@
-
--- DDL modification 1 (09.07.2024, 18:55)
--- Alter the started_at column to be NOT NULL
-ALTER TABLE public.sessions
-ALTER COLUMN started_at SET NOT NULL;
-
--- Alter the ended_at column to have a default value of CURRENT_TIMESTAMP
-ALTER TABLE public.sessions
-ALTER COLUMN ended_at SET DEFAULT CURRENT_TIMESTAMP;
